50d49c468140a7d15d5c4133c822cc904c86678e
Richard June <rolfanragnorak@gmail.com>
2018-07-08 07:16:32 -0500

    Create project, add Result

---
+++ June.Tools/Functional/Error.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+
+namespace June.Tools.Functional
+{
+    /// <summary>Error when a method that returns <see cref="Result"/> fails.</summary>
+    [DebuggerDisplay("{Message}")]
+    public class Error
+    {
+        /// <summary>Error message indicating why the method failed.</summary>
+        public string Message { get; set; }
+
+        /// <summary>
+        /// Create a new Error with <paramref name="message"/>
+        /// </summary>
+        /// <param name="message"></param>
+        public Error(string message)
+        {
+            Message = message;
+        }
+
+        /// <summary>
+        /// Returns only the <see cref="Message"/> property
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return Message;
+        }
+    }
+}
